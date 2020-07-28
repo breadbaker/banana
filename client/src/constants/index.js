63e4069acef045b3e5bee640ffba6bed307715ef
Dan Baker <danbaker@Dans-MBP-2.lan>
2020-07-28 12:49:27 -0400

    test-amp

--- client/src/constants/index.js
+++
@@ -1,2 +0,0 @@
-export const FLIGHTS_LIST = 'FLIGHTS_LIST'
-export const ADD_FLIGHT = 'ADD_FLIGHT'
--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import flights from './flights'
-
-const rootReducer = combineReducers({
-  flights
-})
-
-export default rootReducer
